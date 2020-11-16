85de2756b75270e06156ba9f892353b8902da344
thanhvan <nvthanh061200@gmail.com>
2020-11-17 00:35:05 +0700

    ahi

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const morgan=require('morgan');
+const path=require('path');
+const handlebars  = require('express-handlebars');
+const app=express();
+const port=3000
+
+//http logger
+app.use(morgan('combined'))
+//----------
+
+//teamplate engine
+app.engine('hbs', handlebars({
+    extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+//----------------
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+app.get('/news',(req,res)=>{
+    res.render('news');
+})
+
+app.listen(port,()=>{"example app listening at 3000"}); 
